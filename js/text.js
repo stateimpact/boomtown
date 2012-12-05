967ae3f01e93e3c39003c9dd5839942fd7ee2db1
Wesley Lindamood <wesley.lindamood@gmail.com>
2012-12-05 14:47:56 -0500

    adding new images to slideshow, cleaning up file names, css and extending end of transitions

--- js/text.js
+++ js/text.js
@@ -27,7 +27,7 @@ target:'chapter1'
 
 pop.text({
 start: 15,
-end: 30,
+end: 32,
 text:'<div id="boom-intro3" class="intro animated fadeIn"><h2>The latest boom started in 2007</h2><p>This time: It\'s natural gas. The surrounding area of northeast Pennsylvania is now home to more natural gas wells than any other part of the state. For better or worse, life in town has changed.</p> <p>And for Towandans, one question looms: Does every boom have a bust?</p></div>',
 target:'chapter1'
 });
@@ -52,21 +52,20 @@ pop.code({
 	}
 });
 
-//delayed loading of background images con edits: s1b, s2b, s3b, s4b, s4c, s6b, s8b, s11b, s13b, s13c, s15b, s15c, s20, s20d, s33a, s33b, s33c, s39d, s39e,s39f
+//delayed loading of background images
 pop.code({
 	start: 10,
 	onStart: function( options ) {  
 		$('#s1').css('background-image', 'url(' + 'img/nostalgia1d.jpg' + ')');
-		$('#s1b').css('background-image', 'url(' + 'img/sleepy.jpg' + ')');
+		$('#s1b').css('background-image', 'url(' + 'img/antique-car.jpg' + ')');
 		$('#s2').css('background-image', 'url(' + 'img/chief-photo.jpg' + ')');
 		$('#s2b').css('background-image', 'url(' + 'img/boro.jpg' + ')');
 		$('#s3').css('background-image', 'url(' + 'img/shale-well1a.jpg' + ')');
 		$('#s3a').css('background-image', 'url(' + 'img/shale-well2.jpg' + ')');
-		$('#s3b').css('background-image', 'url(' + 'img/rural.jpg' + ')');
+		$('#s3b').css('background-image', 'url(' + 'img/morse.jpg' + ')');
 		$('#s4').css('background-image', 'url(' + 'img/old-towanda.jpg' + ')');
 		$('#s4b').css('background-image', 'url(' + 'img/truck3.jpg' + ')');
 		$('#s4c').css('background-image', 'url(' + 'img/epler.jpg' + ')');
-		//con2 added
 		$('#s4c').css('background-image', 'url(' + 'img/old-towanda2.jpg' + ')');
 		$('#s5').css('background-image', 'url(' + 'img/mailboxes.jpg' + ')');
 	}
@@ -82,10 +81,9 @@ pop.code({
 		$('#s9').css('background-image', 'url(' + 'img/musician.jpg' + ')');
 		$('#s10').css('background-image', 'url(' + 'img/restaurant-wide-shot.jpg' + ')');
 		$('#s11').css('background-image', 'url(' + 'img/courthouse.jpg' + ')');
-		//con2 added
-		$('#s11c').css('background-image', 'url(' + 'img/land.jpg' + ')');
+		$('#s11c').css('background-image', 'url(' + 'img/farm-house.jpg' + ')');
 		$('#s11b').css('background-image', 'url(' + 'img/headshot.jpg' + ')');
-		$('#s11a').css('background-image', 'url(' + 'img/truck-flag.jpg' + ')');
+		$('#s11a').css('background-image', 'url(' + 'img/aerial-window.jpg' + ')');
 		$('#s12').css('background-image', 'url(' + 'img/menu.jpg' + ')');
 		$('#s13').css('background-image', 'url(' + 'img/jewelery1.jpg' + ')');
 		$('#s13b').css('background-image', 'url(' + 'img/necklace.jpg' + ')');
@@ -100,12 +98,11 @@ pop.code({
 		$('#s15b').css('background-image', 'url(' + 'img/jan.jpg' + ')');
 		$('#s15c').css('background-image', 'url(' + 'img/oldboots.jpg' + ')');
 		$('#s16').css('background-image', 'url(' + 'img/shoe-store-boots.jpg' + ')');
-		//con stopped here
 		$('#s18').css('background-image', 'url(' + 'img/landscape.jpg' + ')');
 		$('#s19').css('background-image', 'url(' + 'img/chesapeake.jpg' + ')');
 		$('#s20').css('background-image', 'url(' + 'img/marquee.jpg' + ')');
 		$('#s20a').css('background-image', 'url(' + 'img/gtp-manufacturing.jpg' + ')');
-		$('#s20b').css('background-image', 'url(' + 'img/outsiders.jpg' + ')');
+		$('#s20b').css('background-image', 'url(' + 'img/drill-bit-jewelery.jpg' + ')');
 		$('#s20c').css('background-image', 'url(' + 'img/dupont.jpg' + ')');
 		$('#s20d').css('background-image', 'url(' + 'img/spanish.jpg' + ')');
 		$('#s21').css('background-image', 'url(' + 'img/welcome-towanda.jpg' + ')');
@@ -120,11 +117,9 @@ pop.code({
 	onStart: function( options ) {  
 		$('#s27').css('background-image', 'url(' + 'img/classified.jpg' + ')');
 		$('#s28').css('background-image', 'url(' + 'img/joe-benjamin.jpg' + ')');
-		//con2 added
 		$('#s28b').css('background-image', 'url(' + 'img/rent2.jpg' + ')');
 		$('#s29').css('background-image', 'url(' + 'img/grace1.jpg' + ')');
 		$('#s30').css('background-image', 'url(' + 'img/grace2.jpg' + ')');
-		//con2 added
 		$('#s30b').css('background-image', 'url(' + 'img/grace3.jpg' + ')');
 		$('#s31').css('background-image', 'url(' + 'img/taco-food.jpg' + ')');
 		$('#s31a').css('background-image', 'url(' + 'img/food-donations.jpg' + ')');
@@ -133,8 +128,6 @@ pop.code({
 		$('#s33a').css('background-image', 'url(' + 'img/truckseq1.jpg' + ')');
 		$('#s33b').css('background-image', 'url(' + 'img/truckseq2.jpg' + ')');
 		$('#s33c').css('background-image', 'url(' + 'img/truckseq3.jpg' + ')');
-		$('#s34').css('background-image', 'url(' + 'img/truck2.jpg' + ')');
-		$('#s36').css('background-image', 'url(' + 'img/truck4.jpg' + ')');
 
 	}
 });
@@ -142,18 +135,18 @@ pop.code({
 pop.code({
 	start: 50,
 	onStart: function( options ) {
+		$('#s34').css('background-image', 'url(' + 'img/truck2.jpg' + ')');
+		$('#s36').css('background-image', 'url(' + 'img/truck4.jpg' + ')');
 		$('#s37').css('background-image', 'url(' + 'img/old-man.jpg' + ')');
 		$('#s38').css('background-image', 'url(' + 'img/slow-downtown.jpg' + ')');
-		//con2 added
-		$('#s38b').css('background-image', 'url(' + 'img/slow.jpg' + ')');
+		$('#s38b').css('background-image', 'url(' + 'img/middle-mainstreet.jpg' + ')');
 		$('#s39').css('background-image', 'url(' + 'img/restaurant-outside.jpg' + ')');
 		$('#s39b').css('background-image', 'url(' + 'img/headshot.jpg' + ')');
 		$('#s39c').css('background-image', 'url(' + 'img/jan.jpg' + ')');
 		$('#s39d').css('background-image', 'url(' + 'img/bootstore.jpg' + ')');
 		$('#s39e').css('background-image', 'url(' + 'img/gone.jpg' + ')');
-		$('#s39f').css('background-image', 'url(' + 'img/road.jpg' + ')');
+		$('#s39f').css('background-image', 'url(' + 'img/highway.jpg' + ')');
 		$('#s39g').css('background-image', 'url(' + 'img/shale-well1a.jpg' + ')');
-		//con2 added
 		$('#s41a').css('background-image', 'url(' + 'img/burn.jpg' + ')');
 		$('#s41').css('background-image', 'url(' + 'img/foggy-highway.jpg' + ')');
 	}
@@ -162,16 +155,16 @@ pop.code({
 //s1
 pop.text({
 start: 30,
-end: 33,
+end: 35,
 text:'<div id="s1" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
+
 //s1b
 pop.text({
 start: 33,
-end: 40,
+end: 42,
 text:'<div id="s1b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -179,16 +172,16 @@ target:'chapter1'
 //s2
 pop.text({
 start: 40,
-end: 45,
+end: 47,
 text:'<div id="s2" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
+
 //s2b
 pop.text({
 start: 45,
-end: 51,
+end: 53,
 text:'<div id="s2b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -196,7 +189,7 @@ target:'chapter1'
 //s3
 pop.text({
 start: 51,
-end: 55,
+end: 57,
 text:'<div id="s3" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -205,7 +198,7 @@ target:'chapter1'
 
 pop.text({
 start: 55,
-end: 67,
+end: 69,
 text:'<div id="s3a" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -247,20 +240,20 @@ text:'<div class="map"><img src="img/map12.png" alt="2012"><p>2012: Drilled well
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
+
 //s3b
 pop.text({
 start: 67,
-end: 72,
+end: 74,
 text:'<div id="s3b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
+
 //s4b
 pop.text({
 start: 72,
-end: 77,
+end: 79,
 text:'<div id="s4b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -271,16 +264,16 @@ target:'chapter1'
 
 pop.text({
 start: 77,
-end: 86,
+end: 88,
 text:'<div id="s4" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
+
 //s4c
 pop.text({
 start: 86,
-end: 91,
+end: 93,
 text:'<div id="s4c" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -289,7 +282,7 @@ target:'chapter1'
 
 pop.text({
 start: 91,
-end: 97,
+end: 99,
 text:'<div id="s5" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -299,16 +292,16 @@ target:'chapter1'
 
 pop.text({
 start: 97,
-end: 101,
+end: 103,
 text:'<div id="s6" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
+
 //sb6
 pop.text({
 start: 101,
-end: 105,
+end: 107,
 text:'<div id="s6b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -317,26 +310,15 @@ target:'chapter1'
 
 pop.text({
 start: 105,
-end: 110,
+end: 112,
 text:'<div id="s7" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-//s8
-/*
-pop.text({
-start: 112,
-end: 121,
-text:'<div id="s8" class="segment animated fadeIn"></div>',
-target:'chapter1'
-});
-*/
-
-/*CON ADDED THIS */
 //s8b
 pop.text({
 start: 110,
-end: 113,
+end: 115,
 text:'<div id="s8b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -345,7 +327,7 @@ target:'chapter1'
 
 pop.text({
 start: 113,
-end: 116,
+end: 118,
 text:'<div id="s9" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -354,7 +336,7 @@ target:'chapter1'
 
 pop.text({
 start: 116,
-end: 124,
+end: 126,
 text:'<div id="s10" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -363,7 +345,7 @@ target:'chapter1'
 
 pop.text({
 start: 124,
-end: 130,
+end: 132,
 text:'<div id="s11" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -371,16 +353,15 @@ target:'chapter1'
 //s11c
 pop.text({
 start: 130,
-end: 138,
+end: 140,
 text:'<div id="s11c" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
 //s11b
 pop.text({
 start: 137,
-end: 142,
+end: 144,
 text:'<div id="s11b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -389,7 +370,7 @@ target:'chapter1'
 
 pop.text({
 start: 142,
-end: 152,
+end: 154,
 text:'<div id="s11a" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -397,7 +378,7 @@ target:'chapter1'
 //s12
 pop.text({
 start: 152,
-end: 158,
+end: 160,
 text:'<div id="s12"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -405,25 +386,23 @@ target:'chapter1'
 //s13
 pop.text({
 start: 158,
-end: 162,
+end: 164,
 text:'<div id="s13"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
 //s13b
 pop.text({
 start: 162,
-end: 166,
+end: 168,
 text:'<div id="s13b"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
 //s13c
 pop.text({
 start: 166,
-end: 170,
+end: 172,
 text:'<div id="s13c"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -431,25 +410,23 @@ target:'chapter1'
 //s15
 pop.text({
 start: 170,
-end: 174,
+end: 176,
 text:'<div id="s15"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
 //s15b
 pop.text({
 start: 174,
-end: 178,
+end: 180,
 text:'<div id="s15b"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED THIS */
 //s15c
 pop.text({
 start: 178,
-end: 184,
+end: 186,
 text:'<div id="s15c"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -457,17 +434,15 @@ target:'chapter1'
 //s16
 pop.text({
 start: 184,
-end: 193,
+end: 195,
 text:'<div id="s16"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON STOPPED HERE */
-
 //s18
 pop.text({
 start: 193,
-end: 201,
+end: 203,
 text:'<div id="s18"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -483,7 +458,7 @@ target:'chapter1'
 //s19
 pop.text({
 start: 201,
-end: 205,
+end: 207,
 text:'<div id="s19"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -491,16 +466,16 @@ target:'chapter1'
 //s20
 pop.text({
 start: 205,
-end: 210,
+end: 212,
 text:'<div id="s20"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON MOVED THE NEXT THREE AROUND AND EDITED TIME */
+
 //s20a
 pop.text({
 start: 210,
-end: 215,
+end: 217,
 text:'<div id="s20a"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -508,7 +483,7 @@ target:'chapter1'
 //s20c
 pop.text({
 start: 215,
-end: 222,
+end: 224,
 text:'<div id="s20c"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -516,18 +491,15 @@ target:'chapter1'
 //s20b
 pop.text({
 start: 222,
-end: 228,
+end: 230,
 text:'<div id="s20b"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-
-
-/*CON ADDED IMAGE AND EDITED TIME */
 //s20d
 pop.text({
 start: 228,
-end: 233,
+end: 235,
 text:'<div id="s20d"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -535,7 +507,7 @@ target:'chapter1'
 //s21
 pop.text({
 start: 233,
-end: 248,
+end: 250,
 text:'<div id="s21"  class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -551,7 +523,7 @@ target:'chapter1'
 //s23
 pop.text({
 start: 248,
-end: 256,
+end: 258,
 text:'<div id="s23" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -559,7 +531,7 @@ target:'chapter1'
 //s24
 pop.text({
 start: 256,
-end: 264,
+end: 266,
 text:'<div id="s24" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -567,7 +539,7 @@ target:'chapter1'
 //s26
 pop.text({
 start: 264,
-end: 269,
+end: 271,
 text:'<div id="s26" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -575,7 +547,7 @@ target:'chapter1'
 //s27-- classified
 pop.text({
 start: 269,
-end: 289,
+end: 291,
 text:'<div id="s27" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -607,7 +579,7 @@ target:'chapter1'
 //s28
 pop.text({
 start: 287,
-end: 295,
+end: 297,
 text:'<div id="s28" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -615,7 +587,7 @@ target:'chapter1'
 //s28b
 pop.text({
 start: 295,
-end: 303,
+end: 305,
 text:'<div id="s28b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -623,7 +595,7 @@ target:'chapter1'
 //s29
 pop.text({
 start: 303,
-end: 311,
+end: 313,
 text:'<div id="s29" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -631,7 +603,7 @@ target:'chapter1'
 //s30
 pop.text({
 start: 311,
-end: 320,
+end: 322,
 text:'<div id="s30" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -639,7 +611,7 @@ target:'chapter1'
 //s30b
 pop.text({
 start: 320,
-end: 326,
+end: 328,
 text:'<div id="s30b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -647,7 +619,7 @@ target:'chapter1'
 //s31
 pop.text({
 start: 326,
-end: 332,
+end: 334,
 text:'<div id="s31" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -655,7 +627,7 @@ target:'chapter1'
 //s31a
 pop.text({
 start: 332,
-end: 341,
+end: 343,
 text:'<div id="s31a" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -663,7 +635,7 @@ target:'chapter1'
 //s32
 pop.text({
 start: 341,
-end: 345,
+end: 347,
 text:'<div id="s32" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -671,7 +643,7 @@ target:'chapter1'
 //s33a
 pop.text({
 start: 345,
-end: 347,
+end: 349,
 text:'<div id="s33a" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -679,7 +651,7 @@ target:'chapter1'
 //s33b
 pop.text({
 start: 347,
-end: 349,
+end: 351,
 text:'<div id="s33b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -687,7 +659,7 @@ target:'chapter1'
 //s33c
 pop.text({
 start: 349,
-end: 351,
+end: 353,
 text:'<div id="s33c" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -695,7 +667,7 @@ target:'chapter1'
 //s33
 pop.text({
 start: 351,
-end: 356,
+end: 358,
 text:'<div id="s33" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -703,7 +675,7 @@ target:'chapter1'
 //s34
 pop.text({
 start: 356,
-end: 361,
+end: 363,
 text:'<div id="s34" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -711,7 +683,7 @@ target:'chapter1'
 //s36
 pop.text({
 start: 361,
-end: 366,
+end: 368,
 text:'<div id="s36" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -719,7 +691,7 @@ target:'chapter1'
 //s37
 pop.text({
 start: 366,
-end: 372,
+end: 374,
 text:'<div id="s37" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -727,7 +699,7 @@ target:'chapter1'
 //s38
 pop.text({
 start: 372,
-end: 376,
+end: 378,
 text:'<div id="s38" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -735,7 +707,7 @@ target:'chapter1'
 //s38b
 pop.text({
 start: 376,
-end: 385,
+end: 387,
 text:'<div id="s38b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -743,34 +715,31 @@ target:'chapter1'
 //s39
 pop.text({
 start: 385,
-end: 390,
+end: 392,
 text:'<div id="s39" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON RECYCLED IMAGE FROM EARLIER */
 //s39b
 pop.text({
 start: 390,
-end: 396,
+end: 398,
 text:'<div id="s39b" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED RECYCLED IMAGE FROM EARLIER */
 //s39c
 pop.text({
 start: 396,
-end: 402,
+end: 404,
 text:'<div id="s39c" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED NEXT 3 IMAGES SORRY */
 //s39d
 pop.text({
 start: 402,
-end: 409,
+end: 411,
 text:'<div id="s39d" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -778,7 +747,7 @@ target:'chapter1'
 //s39e
 pop.text({
 start: 409,
-end: 416,
+end: 418,
 text:'<div id="s39e" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -786,16 +755,15 @@ target:'chapter1'
 //s39f
 pop.text({
 start: 416,
-end: 424,
+end: 426,
 text:'<div id="s39f" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
 
-/*CON ADDED RECYCLED IMAGE FROM EARLIER */
 //s39g
 pop.text({
 start: 424,
-end: 432,
+end: 434,
 text:'<div id="s39g" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -803,7 +771,7 @@ target:'chapter1'
 //s41a
 pop.text({
 start: 432,
-end: 439,
+end: 441,
 text:'<div id="s41a" class="segment animated fadeIn"></div>',
 target:'chapter1'
 });
@@ -817,7 +785,6 @@ target:'chapter1'
 });
 
 //s42
-
 pop.text({
 start: 441,
 end: 452,
